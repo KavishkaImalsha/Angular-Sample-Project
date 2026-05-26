2fb4a21d0cc511268a78753a172a48de077513ca
Kavishka Imalsha <kavishkaimalsha@gmail.com>
2026-05-26 12:05:25 +0530

    How to define variables and funtions

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  userName = "test user"
+  userId = 5521435
+  userbio = {
+    id: 23213,
+    description: "Web Developer"
+  }
+  email: String
+
+  constructor() { 
+    this.email = "testuser@gmail.com"
+  }
+
+  showName() { 
+    return this.userName
+  }
+}
